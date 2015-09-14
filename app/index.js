a4df2c1bb7dd71f49f085812758fe1cfbb39aea9
Andrew Hart <andrew.hart@rewardstyle.com>
2015-09-14 14:58:11 -0500

    establishing initial logic for prompts

--- app/index.js
+++ app/index.js
@@ -1,9 +1,38 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+var yeoman = require('yeoman-generator'),
+    chalk = require('chalk'),
+    yosay = require('yosay'),
+    _     = require('underscore.string'),
+    angularUtils = require('../util');
+
 
 module.exports = yeoman.generators.Base.extend({
+  constructor: function() {
+    yeoman.generators.Base.apply(this, arguments);
+    this.argument('appname', { type: String, required: true });
+
+    this.appname = this.appname || path.basename(process.cwd());
+    this.appname = _.camelize(_.slugify(_.humanize(this.appname)));
+
+    if (typeof this.env.options.appPath === 'undefined') {
+      this.option('appPath', {
+        desc: 'Allow to choose where to write the files'
+      });
+
+      this.env.options.appPath = this.options.appPath;
+
+      if (!this.env.options.appPath) {
+        try {
+          this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
+        } catch (e) {}
+      }
+      this.env.options.appPath = this.env.options.appPath || 'app';
+      this.options.appPath = this.env.options.appPath;
+    }
+
+    this.appPath = this.env.options.appPath;
+  },
+
   prompting: function () {
     var done = this.async();
 
@@ -12,16 +41,51 @@ module.exports = yeoman.generators.Base.extend({
       'Welcome to the outstanding ' + chalk.red('GeneratorRsAngular') + ' generator!'
     ));
 
+        this.option('app-suffix', {
+      desc: 'Allow a custom suffix to be added to the module name',
+      type: String
+    });
+    this.env.options['app-suffix'] = this.options['app-suffix'];
+    this.scriptAppName = this.appname + angularUtils.appName(this);
+
+
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'rawlist',
+      name: 'moduleType',
+      message: 'What kind of module is this?',
+      choices: [
+      {
+        value: 'core',
+        name: 'core',
+        checked: false
+      },
+      {
+        value: 'utility',
+        name: 'utility',
+        checked: false
+      },
+      {
+        value: 'collection',
+        name: 'colleciton',
+        checked: false
+      },
+      {
+        value: 'app',
+        name: 'app',
+        checked: false
+      },
+      {
+        value: 'custom',
+        name: 'custom',
+        checked: false
+      }]
     }];
 
     this.prompt(prompts, function (props) {
       this.props = props;
       // To access props later use this.props.someOption;
+
+      this.log("You chose " + this.props.moduleType);
 
       done();
     }.bind(this));
@@ -51,7 +115,13 @@ module.exports = yeoman.generators.Base.extend({
     }
   },
 
-  install: function () {
-    this.installDependencies();
+  install: function() {
+    // Change working directory to 'src' for dependency install
+    // var npmDir = process.cwd() + '/src';
+    // process.chdir(npmDir);
+
+    // this.installDependencies();
+    this.spawnCommand("npm", ["install"], {cwd: 'src'});
+    this.spawnCommand("bower", ["install"], {cwd: 'src'});
   }
 });
