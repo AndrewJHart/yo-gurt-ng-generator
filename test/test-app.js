46e18a0bbb9fbc35cb831d45629f47f03d214a2f
Andrew Hart <andrew.hart@rewardstyle.com>
2015-09-22 11:27:15 -0500

    writing tests

--- test/test-app.js
+++ test/test-app.js
@@ -5,20 +5,25 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 var os = require('os');
 
-describe('generator rs angular:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
-      .on('end', done);
-  });
+/**
+ * Unit tests for the core app generator
+ */
+describe('rs-angular app', function() {
+  describe('generator rs angular:app', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withOptions({ skipInstall: true })
+        .withPrompts({ someOption: true })
+        .on('end', done);
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
-    ]);
+    it('creates files', function () {
+      assert.file([
+        'bower.json',
+        'package.json',
+        '.editorconfig',
+        '.jshintrc'
+      ]);
+    });
   });
 });
